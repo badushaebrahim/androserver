b1e84c84901090236ea3d15fef512689fd5dd5b9
BADU\badus <badushaebrahim62@gmail.com>
2022-03-05 12:51:11 +0530

    resistration of user fixed niot latitude nad longitude data

--- regapi.php
+++ regapi.php
@@ -7,18 +7,21 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 	$name = $_POST['name'];
     $email = validate($_POST['email']);
     $password = validate($_POST['password']);
-$lat1= $_POST['lati'];
-$lati=10.15445;
+//$lat1= $_POST['lati'];
+$lati1=10.15445;
+$lati=floatval($lati1);
 //floatval($lat1);
 $longi2=75.155;
 //$_POST['longi'];
-$longi=75.15245;
-//floatval($longi2);
+$longi=floatval($longi2);
     $Num2 = $_POST['number'];
     $Num= intval($Num2);
 	$address = $_POST['address'];
     // Create the SQL query string. We'll use md5() function for data security. It calculates and returns the MD5 hash of a string
+
     $sql="INSERT INTO `users`( `name`, `email`, `password`, `Number`, `address`, `lat`, `longi`) VALUES ('$name','$email','$password','$Num','$address',$lati,$longi)";
+
+
     // Execute the query. Print "success" on a successful execution, otherwise "failure".
 //echo "$sql";
     if(!$conn->query($sql)){
