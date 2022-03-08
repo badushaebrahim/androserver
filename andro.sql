fd28153321f11968e9220c567a17032cf1202fae
BADU\badus <badushaebrahim62@gmail.com>
2022-03-08 23:07:23 +0530

    updated database backup

--- andro.sql
+++ andro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 06, 2022 at 06:20 PM
+-- Generation Time: Mar 08, 2022 at 06:36 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `andro`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `msgholder`
+--
+
+CREATE TABLE `msgholder` (
+  `msg` text NOT NULL,
+  `senterid` int(11) NOT NULL,
+  `reciverid` int(11) NOT NULL,
+  `prodid` int(11) NOT NULL,
+  `prodname` text NOT NULL,
+  `sentername` text NOT NULL,
+  `msgid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `msgholder`
+--
+
+INSERT INTO `msgholder` (`msg`, `senterid`, `reciverid`, `prodid`, `prodname`, `sentername`, `msgid`) VALUES
+('Messagehello', 1, 94, 14, 'makes', 'badu', 1),
+('hi bro ', 94, 1, 14, 'makes', 'bar', 2),
+('hello', 94, 2, 12, 'jake', 'bar', 3),
+('hello', 94, 2, 12, 'jake', 'bar', 4),
+('hellobro', 94, 2, 12, 'jake', 'bar', 5),
+('this is test', 94, 2, 12, 'jake', 'bar', 6),
+('this', 94, 2, 12, 'jake', 'bar', 7),
+('ramaen', 94, 2, 12, 'jake', 'bar', 8);
 
 -- --------------------------------------------------------
 
@@ -57,7 +87,9 @@ INSERT INTO `sellings` (`type`, `Listing_title`, `imagelink`, `long_details`, `c
 ('Product', 'add', 'IMG1406132153.jpg', 'add', '95446600', 0, 0, 'oorapra house chowara po', 1, 9, '5'),
 ('Product', 'tapa', 'IMG717945658.jpg', 'tapa', '95446600', 10.5642, 75.1657, 'oorapra house chowara po', 1, 10, '77'),
 ('Service', 'ser', 'IMG1887239032.jpg', 'serv', '95446600', 10.5642, 75.1657, 'oorapra house chowara po', 1, 11, '6'),
-('Product', 'jake', 'IMG267382299.jpg', 'jake', '9746097187', 10.1545, 75.155, 'this is at my home', 2, 12, '66');
+('Product', 'jake', 'IMG267382299.jpg', 'jake', '9746097187', 10.1545, 75.155, 'this is at my home', 2, 12, '66'),
+('Product', 'ulst', 'IMG2019043933.jpg', 'story', '8943586297', 10.1545, 75.155, 'thi is ults', 6, 13, '78'),
+('Product', 'makes', 'IMG337371152.jpg', 'mak', '95769481388', 10.1037, 76.3635, 'thi is address of 123', 94, 14, '8');
 
 -- --------------------------------------------------------
 
@@ -84,11 +116,19 @@ INSERT INTO `users` (`name`, `email`, `password`, `Number`, `address`, `lat`, `l
 ('badu', 'bad@g.com', 'badusha112', '95446600', 'oorapra house chowara po', 10.5642, 75.1657, 1),
 ('badu', 'jake@j.com', 'badusha112', '9746097187', 'this is at my home', 10.1545, 75.155, 2),
 ('', 'bad@g2.com', 'badusha112', '954465464', 'this', 10.1545, 75.155, 3),
-('', 'bad@g2.com', 'badusha112', '954465464', 'this', 10.1545, 75.155, 4);
+('', 'bad@g2.com', 'badusha112', '954465464', 'this', 10.1545, 75.155, 4),
+('new', 'bb@b.com', 'badbadbad', '9544655941', 'thi is my address', 10.1545, 75.155, 5),
+('bar', 'b@b.com', '12345678', '95769481388', 'thi is address of 123', 10.1037, 76.3635, 94);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `msgholder`
+--
+ALTER TABLE `msgholder`
+  ADD PRIMARY KEY (`msgid`);
 
 --
 -- Indexes for table `sellings`
@@ -107,16 +147,22 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `msgholder`
+--
+ALTER TABLE `msgholder`
+  MODIFY `msgid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
 -- AUTO_INCREMENT for table `sellings`
 --
 ALTER TABLE `sellings`
-  MODIFY `Lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Lid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=95;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
