726bb880ea1a48ed053e19261f00ffad880faccf
BADU\badus <badushaebrahim62@gmail.com>
2022-03-08 21:54:03 +0530

    msg fetching

--- getmsg.php
+++ getmsg.php
@@ -1,3 +1,13 @@
 <?php
+include 'con22.php';
+$me=$_GET['me'];
+$pid=$_GET['pid'];
+$rid=$_GET['rid'];
+$qry="SELECT * FROM `msgholder` WHERE `senterid`=$me AND `reciverid`=$rid OR `senterid`=$rid AND `reciverid`=$me AND `prodid`=$pid;";
+$r= mysqli_query($conn,$qry);
+while($row = mysqli_fetch_assoc($r)){
+$jso[] = $row; 
+}
+print(json_encode($jso));
 
 ?>
